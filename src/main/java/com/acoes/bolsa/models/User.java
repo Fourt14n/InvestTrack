6dc6f9c1009f6b433c4273839e7000c4e8bdff98
fourt14n <hebertsep1914@gmail.com>
2025-03-14 20:02:40 -0300

    Iniciando o projeto

---
+++ src/main/java/com/acoes/bolsa/models/User.java
@@ -0,0 +1,10 @@
+package com.acoes.bolsa.models;
+
+class User {
+  @Id
+  @GeneratedValue(strategy = Gener)
+private Integer id;
+private String name;
+private String email;
+  
+}
---
+++ src/main/java/com/acoes/bolsa/models/acoesModel.java
@@ -0,0 +1,12 @@
+package com.acoes.bolsa.models;
+
+import java.util.List;
+
+public class acoesModel {
+
+    public class Stock
+
+    public List<Stock> Stocks; 
+
+
+}
